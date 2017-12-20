a1beca786d5c52471877ff7450307d31334015a6
andwhi <andrew.whitten@icloud.com>
2017-12-19 20:13:50 -0500

    Add files

---
+++ Film.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Treehouse.PracticeSession
+{
+
+    class Film
+    {
+
+        public string title;
+        public string year;
+
+        public Film(string title, string year)
+        {
+            this.title = title;
+            this.year = year;
+        }
+
+
+
+    }
+}
---
+++ Filmography.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Treehouse.PracticeSession
+{
+
+    class Filmography
+    {
+        List<Film> films;
+
+        public Filmography() => films = new List<Film>();
+
+        public bool AddFilm(string title, string year)
+        {
+            Film entry = new Film(title, year);
+            Film result = FindFilm(title);
+
+            if (result == null)
+            {
+                films.Add(entry);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool RemoveFilm(string title)
+        {
+            Film entry = FindFilm(title);
+
+            if (entry != null)
+            {
+                films.Remove(entry);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void ListFilm(Action<Film> action)
+        {
+            films.ForEach(action); //what is this?
+        }
+
+        public bool IsEmpty()
+        {
+            return (films.Count == 0);
+        }
+
+        public Film FindFilm(string title)
+        {
+            Film entry = films.Find(
+              delegate (Film a) {
+                  return a.title == title;  // what is this?
+        }
+            );
+            return entry;
+        }
+    }
+}
